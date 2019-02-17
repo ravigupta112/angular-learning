2d94f3080d826a82fe6368437d8b784e6825b69c
Ravi Gupta <ravigupta112>
2019-02-17 15:33:58 +0530

    add style, clss, ifcondition and for loop

--- src/app/server/sever.component.ts
+++ src/app/server/sever.component.ts
@@ -8,8 +8,46 @@ import {Component} from '@angular/core';
 export class ServerComponent{
    serverId  = '10';
    serverStatus = 'offline'; 
-   
+   logItem = [];
    getServerStatus(){
        return this.serverStatus;
    }
+
+   toggleButton(event){
+    this.logItem.push(new Date());
+   }
+
+   getColor(country) {
+    switch (country) {
+      case "UK":
+        return "green";
+      case "USA":
+        return "blue";
+      case "HK":
+        return "red";
+    }
+  }
+
+  people: any[] = [
+    {
+      name: "Douglas  Pace",
+      country: "UK"
+    },
+    {
+      name: "Mcleod  Mueller",
+      country: "USA"
+    },
+    {
+      name: "Day  Meyers",
+      country: "HK"
+    },
+    {
+      name: "Aguirre  Ellis",
+      country: "UK"
+    },
+    {
+      name: "Cook  Tyson",
+      country: "USA"
+    }
+  ];
 }
